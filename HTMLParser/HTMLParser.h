0ea003979d28c19063ad8e2c4e5723d05e9447f9
Raymond McCrae <raymccrae@yahoo.com>
2017-07-20 18:24:45 +0100

    Create empty Cocoa Touch Framework project files

---
+++ HTMLParser/HTMLParser.h
@@ -0,0 +1,19 @@
+//
+//  HTMLParser.h
+//  HTMLParser
+//
+//  Created by Raymond Mccrae on 20/07/2017.
+//  Copyright © 2017 Raymond Mccrae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTMLParser.
+FOUNDATION_EXPORT double HTMLParserVersionNumber;
+
+//! Project version string for HTMLParser.
+FOUNDATION_EXPORT const unsigned char HTMLParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTMLParser/PublicHeader.h>
+
+
